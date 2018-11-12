83ad0e152e68423658d5833ef2a0543a5a117312
farispee <farisp@live.com>
2018-11-12 19:43:09 +0800

    changes

--- android/src/main/java/fr/greweb/rnwebgl/RNWebGLViewManager.java
+++ android/src/main/java/fr/greweb/rnwebgl/RNWebGLViewManager.java
@@ -1,8 +1,15 @@
 package fr.greweb.rnwebgl;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 import java.util.Map;
 
@@ -11,20 +18,50 @@ import javax.annotation.Nullable;
 public class RNWebGLViewManager extends SimpleViewManager<RNWebGLView> {
   public static final String REACT_CLASS = "RNWebGLView";
 
+  public static final int COMMAND_SAVE_IMAGE = 1;
+  public static final int COMMAND_RESET_IMAGE = 2;
+
   @Override
   public String getName() {
     return REACT_CLASS;
   }
-
+  public static final int COMMAND_CAPTURE_FRAME = 1;
   @Override
   public RNWebGLView createViewInstance(ThemedReactContext context) {
     return new RNWebGLView(context);
   }
 
   @Override
+  public void receiveCommand( RNWebGLView rnWebGLView ,int commandType, @Nullable ReadableArray args) {
+    Assertions.assertNotNull(rnWebGLView);
+    Assertions.assertNotNull(args);
+    switch (commandType) {
+      case COMMAND_CAPTURE_FRAME: {
+        rnWebGLView.saveImage();
+        return;
+      }
+      default:
+        throw new IllegalArgumentException(String.format(
+                "Unsupported command %d received by %s.",
+                commandType,
+                getClass().getSimpleName()));
+    }
+  }
+
+  @Override
   public @Nullable Map getExportedCustomDirectEventTypeConstants() {
     return MapBuilder.of(
             "surfaceCreate",
-            MapBuilder.of("registrationName", "onSurfaceCreate"));
+            MapBuilder.of("registrationName", "onSurfaceCreate"),
+            "capture",
+            MapBuilder.of("registrationName","capture")
+            );
+
+  }
+  @Override
+  public Map<String,Integer> getCommandsMap() {
+    return MapBuilder.of(
+            "capture",
+            COMMAND_CAPTURE_FRAME);
   }
 }
